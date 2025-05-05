3d34795cce9ba4b3c8e5ee5dd78bfb679dabac26
MathisRameau <mathis.rameau@etu.cyu.fr>
2025-05-05 10:44:20 +0200

    Update path_direction.h

--- path_direction.h
+++ path_direction.h
@@ -7,6 +7,6 @@
 #define HAUT 2
 #define DROITE 3
 
-int choix_direction(int derniere_direction, Case case_chemin, int zone_top, int zone_bottom, int zone_left, int zone_right);
+int choix_direction(int derniere_direction, Case case_chemin, int zone_top, int zone_bottom, int zone_left, int zone_right, int width);
 
 #endif //PATH_DIRECTION_H
