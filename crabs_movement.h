75fed93be0e235d334d5671a1b1e364bed2c30a8
MathisRameau <mathis.rameau@etu.cyu.fr>
2025-05-07 11:21:06 +0200

    Delete crabs_movement.h

--- crabs_movement.h
+++
@@ -1,9 +0,0 @@
-#ifndef CRAB_MOVEMENT_H
-#define CRAB_MOVEMENT_H
-
-#include "gameBoard_creation.h"
-#include "crabs_creation.h"
-
-void move_crab(Crab* crab, Case** game_board, Case* chemin, int chemin_length, int* crab_index);
-
-#endif
