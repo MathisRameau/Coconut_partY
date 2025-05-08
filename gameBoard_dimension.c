cd31824069c9c872cccad3f26dde9d4558323268
MathisRameau <mathis.rameau@etu.cyu.fr>
2025-05-08 15:37:41 +0200

    Update gameBoard_dimension.c

--- gameBoard_dimension.c
+++ gameBoard_dimension.c
@@ -5,22 +5,14 @@
 void game_board_dimension(int* height, int* width) {
 
       do {
-            printf("De quelle hauteur veux-tu que le plateau soit ? --> ");
-            if (scanf("%d", height) != 1 || *height <= 10) {
+            printf("De quelle taille veux-tu que le plateau soit ? --> ");
+            if (scanf("%d", height) != 1 || *height <= 20 || *height >= 70) {
 
-                  printf("Veuillez entrer un entier supérieur à 10 pour la hauteur.\n");
+                  printf("\nVeuillez entrer un entier superieur à 20 et inferieur à 70 pour la hauteur.\n\n");
                   while (getchar() != '\n');
                   *height = 0;
             }
       } while (*height <= 0);
 
-      do {
-            printf("De quelle largeur veux-tu que le plateau soit ? --> ");
-            if (scanf("%d", width) != 1 || *width <= 10) {
-
-                  printf("Veuillez entrer un entier supérieur à 10 valide pour la largeur.\n");
-                  while (getchar() != '\n');
-                  *width = 0;
-            }
-      } while (*width <= 0);
+      *width = *height * 2;
 }
