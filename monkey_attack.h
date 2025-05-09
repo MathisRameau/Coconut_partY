e52d22c518abae57b0e5b711d732eef4d183b57e
Lahouari031 <lahouariguetarni@yahoo.com>
2025-05-09 16:39:23 +0200

    monkey_attack.h

---
+++ monkey_attack.h
@@ -0,0 +1,11 @@
+#ifndef MONKEY_ATTACK_H
+#define MONKEY_ATTACK_H
+
+#include "monkeys_creation.h"
+#include "crabs_creation.h"
+#include "gameBoard_creation.h" 
+#include "monkeys_creation.h"
+
+void monkeys_attack_crabs(Monkey* monkeys, int monkey_nombre, Crab* crab, Case** game_board); 
+
+#endif
