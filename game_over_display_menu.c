2fd81e42a1267d1d585b6d79f5565341c6fff2ad
Sara158209 <sara.belghazi@etu.cyu.fr>
2025-05-10 15:02:20 +0200

    game_over_display_menu.c

--- game_over_display_menu.c
+++ game_over_display_menu.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include "game_over_display_menu.h"
 
-void game_over_display_menu(int tour, int coconut_number, char* pseudo) {
-    int choice;
+void game_over_display_menu(int tour, int nb_ndc, char* pseudo) {
+    int choix;
 
 
     printf("\n\n========================================\n");
     printf("            GAME OVER\n");
     printf("========================================\n");
     printf("Tour: %d\n", tour);
-    printf("Nombre de noix de coco gagnees : %d\n", coconut_number);
+    printf("Nombre de noix de coco gagnees : %d\n", nb_ndc);
     printf("========================================\n");
     printf("1. Nouvelle partie\n");
     printf("2. Quitter\n");
@@ -18,13 +18,13 @@ void game_over_display_menu(int tour, int coconut_number, char* pseudo) {
 
     do {
         printf("Votre choix --> ");
-        if(scanf("%d", &choice) != 1 || choice < 1 || choice > 2) {
+        if(scanf("%d", &choix) != 1 || choix < 1 || choix > 2) {
             printf("Choix invalide. Veuillez entrer 1 ou 2.\n");
             while(getchar() != '\n'); // Vider le buffer
         }
-    } while(choice < 1 || choice > 2);
+    } while(choix < 1 || choix > 2);
 
-    if(choice == 1) {
+    if(choix == 1) {
         printf("\nNouvelle partie...\n");
         // Relancer le jeu (ps: essayer de voir comment relancer le jeu)
     } else {
