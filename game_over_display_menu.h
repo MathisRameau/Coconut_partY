b525606876dd28c1d8ebc8d4c74e1cdc7a194bd0
Sara158209 <sara.belghazi@etu.cyu.fr>
2025-05-10 14:54:28 +0200

    game_over_display_menu.h

--- game_over_display_menu.h
+++ game_over_display_menu.h
@@ -1,6 +1,6 @@
 #ifndef GAME_OVER_DISPLAY_MENU_H
 #define GAME_OVER_DISPLAY_MENU_H
 
-void game_over_display_menu(int tour, int coconut_number, char* pseudo);
+void game_over_display_menu(int turn, int coconut_number, char* pseudo);
 
 #endif
